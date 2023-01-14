eef4dcb7384ad269aa40c2bf5cc140a09528d3bc
Kkuil <3024067144@qq.com>
2023-01-14 15:33:05 +0800

    #main layout

--- src/router/index.js
+++ src/router/index.js
@@ -7,12 +7,56 @@ import Lazy from "@/utils/Lazy"
 export default createBrowserRouter([
     {
         path: "/",
-        element: <Navigate to="/MArticle" />,
+        element: <Navigate to="/mArticle/main" />,
     },
     {
-        path: "/MArticle",
+        path: "/mArticle",
         element: <Lazy path="MArticle"/>,
-        loader: loaders.AuthLoader
+        loader: loaders.AuthLoader,
+        children: [
+            {
+                path: "main",
+                element: <Lazy path="MArticle/views/Main"/>
+            },
+            {
+                path: "article",
+                element: <Lazy path="MArticle/views/Article"/>,
+                children: [
+                    {
+                        path: "check",
+                        element: <Lazy path="MArticle/views/Article/views/Check"/>
+                    },
+                    {
+                        path: "articles",
+                        element: <Lazy path="MArticle/views/Article/views/Articles"/>
+                    }
+                ]
+            },
+            {
+                path: "admin",
+                element: <Lazy path="MArticle/views/Admin"/>,
+                children: [
+                    {
+                        path: "right",
+                        element: <Lazy path="MArticle/views/Admin/views/Right" />
+                    },
+                    {
+                        path: "members",
+                        element: <Lazy path="MArticle/views/Admin/views/Members" />
+                    }
+                ]
+            },
+            {
+                path: "user",
+                element: <Lazy path="MArticle/views/User"/>,
+                children: [
+                    {
+                        path: "users",
+                        element: <Lazy path="MArticle/views/User/views/Users" />
+                    }
+                ]
+            },
+        ]
     },
     {
         path: "/login",
--- src/views/MArticle/index.jsx
+++ src/views/MArticle/index.jsx
@@ -1,16 +1,147 @@
-import React, { useEffect } from "react"
-import { useLoaderData } from "react-router-dom"
-import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from "react"
+import { useNavigate, useLoaderData, Outlet, useLocation } from "react-router-dom"
+import { Breadcrumb, Layout, Menu, theme } from "antd"
+import { HeatMapOutlined, UserOutlined, UserSwitchOutlined, EditOutlined } from "@ant-design/icons"
 
+const { Header, Content, Footer, Sider } = Layout
+const items = [
+    {
+        label: "首页",
+        key: "/main",
+        icon: <HeatMapOutlined />
+    },
+    {
+        label: "文章管理",
+        key: "/article",
+        icon: <EditOutlined />,
+        children: [
+            {
+                label: "待审文章",
+                key: "/article/check"
+            },
+            {
+                label: "文章概览",
+                key: "/article/articles"
+            },
+        ]
+    },
+    {
+        label: "管理员管理",
+        key: "/admin",
+        icon: <UserSwitchOutlined />,
+        children: [
+            {
+                label: "权限管理",
+                key: "/admin/right"
+            },
+            {
+                label: "成员管理",
+                key: "/admin/members"
+            }
+        ]
+    },
+    {
+        label: "用户管理",
+        key: "/user",
+        icon: <UserOutlined />,
+        children: [
+            {
+                label: "用户概览",
+                key: "/user/users"
+            },
+        ]
+    }
+]
 export default function MArticle() {
+    const [collapsed, setCollapsed] = useState(false)
     const navigateTo = useNavigate()
+    const { pathname } = useLocation()
     const { status } = useLoaderData()
     useEffect(() => {
-        if(status == 400) {
+        if (status == 400) {
             navigateTo("/login")
         }
     }, [status])
+    useEffect(() => {
+        if(pathname == "/MArticle") {
+            navigateTo("/mArticle/main")
+        }
+    }, [pathname])
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken()
+    function selected({ key }) {
+        navigateTo(`/mArticle${key}`)
+    }
     return (
-        <div>MArticle</div>
+        <Layout
+            style={{
+                minHeight: "100vh",
+            }}
+        >
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div
+                    style={{
+                        height: 45,
+                        margin: 16,
+                    }}
+                    className="flex_center"
+                >
+                    <img
+                        style={{
+                            height: "100%",
+                        }}
+                        src="/images/logo.png"
+                        className="flex_center"
+                    />
+                </div>
+                <Menu
+                    theme="dark"
+                    defaultSelectedKeys={["/main"]}
+                    mode="inline"
+                    items={items}
+                    onSelect={selected}
+                />
+            </Sider>
+            <Layout className="site-layout">
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: "0 16px",
+                    }}
+                >
+                    <Breadcrumb
+                        style={{
+                            margin: "16px 0",
+                        }}
+                    >
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 545,
+                            background: colorBgContainer,
+                        }}
+                    >
+                        <Outlet></Outlet>
+                    </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: "center",
+                    }}
+                >
+                    <p>All right is preserved by Kkuil</p>
+                    <p>In here, Thanks for Antd cooperation providing components support</p>
+                </Footer>
+            </Layout>
+        </Layout>
     )
 }
